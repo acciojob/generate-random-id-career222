72b33435114e04a092a26e4bd01fb92aaeecf86a
career222 <gadedipak1111@gmail.com>
2026-09-04 20:38:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
-  // write your code here
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < l; i++) {
+    let index = Math.floor(Math.random() * characters.length);
+    result += characters[index];
+  }
+
+  return result;
 }
 
 // Do not change the code below.
